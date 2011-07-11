aed15bf63af08671a13458c6dda25af4cd81e1a7
unknown <Reeithaa@.(none)>
2011-07-11 20:10:09 +0100

    Added Lexiconcache singleton class into the Model that will return a list of nagrams

--- Anagrams/Models/Anagram.cs
+++ Anagrams/Models/Anagram.cs
@@ -5,7 +5,36 @@ using System.Web;
 
 namespace Anagrams.Models
 {
-	public class Anagram
+	public interface ILexicon
 	{
+		IEnumerable<string> GetAnagrams(string input);
+	}
+
+	public class LexiconCache : ILexicon
+	{
+		static readonly LexiconCache instance = new LexiconCache();
+
+		static LexiconCache()
+		{
+
+		}
+
+		LexiconCache()
+		{
+
+		}
+
+		public static LexiconCache Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		public IEnumerable<string>  GetAnagrams(string input)
+		{
+ 			throw new NotImplementedException();
+		}
 	}
 }
