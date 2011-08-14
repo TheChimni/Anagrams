45ef7fbf19df596421a01355de991e1f32131f45
Rieethaa <rieethaa@gmail.com>
2011-08-14 21:05:55 +0100

    New ViewMOdel folder added

---
+++ Anagrams/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anagrams.Models.ViewModels
+{
+	 public class IndexViewModel
+	 {
+		  public IndexViewModel()
+		  {
+				Word = String.Empty;
+				Anagrams = new List<string>() { };
+		  }
+		  public string Word { get; set; }
+		  public IEnumerable<string> Anagrams { get; set; }
+	 }
+}
